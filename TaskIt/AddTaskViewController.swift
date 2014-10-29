8f5be6cfd01d511cb37db8251cc04f3d4e7b9ae9
Miguel A. Moreno <miguel@tyba.com>
2014-10-29 18:22:24 +0100

    preparing view to add new tasks as a modal

---
+++ TaskIt/AddTaskViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddTaskViewController.swift
+//  TaskIt
+//
+//  Created by Miguel Angel Moreno Armenteros on 29/10/14.
+//  Copyright (c) 2014 Miguel Angel Moreno Armenteros. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
