288aa118af1ca6dd626f1f13b0b6c16635695d06
Miguel A. Moreno <miguel@tyba.com>
2014-10-29 10:41:34 +0100

    adding fake data to the table view and using the data source functions

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -12,9 +12,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     
+    //array of fake tasks
+    //let taskArray:[[String:String]] = []
+    var taskArray:[Dictionary<String,String>] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let task1: Dictionary<String,String> = ["task": "Comprar chocolate", "description": "Uno negro y otro con leche", "date": "31/10/2014"]
+        let task2: Dictionary<String,String> = ["task": "Pasar la ITV", "description": "En leganés", "date": "03/12/2014"]
+        let task3: Dictionary<String,String> = ["task": "Recoger el traje", "description": "Ir a recoger el traje a la tintorería de la esquina", "date": "07/11/2014"]
+        self.taskArray += [task1, task2, task3]
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +34,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     //UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.taskArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        println(indexPath.row)
+
         var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
-        cell.taskLabel.text = "Study French"
-        cell.descriptionLabel.text = "Verbs in past and present"
-        cell.dateLabel.text = "12/10/2014"
+        let task = self.taskArray[indexPath.row]
+        cell.taskLabel.text = task["task"]
+        cell.descriptionLabel.text = task["description"]
+        cell.dateLabel.text = task["date"]
         return cell
     }
 
